d1d4fad8cd7410c3d191d3e8009e4e47338495ed
dev-ui <codesign.k@gmail.com>
2019-06-09 10:39:59 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9141c2cf4254bf45be15.js"
+    "url": "webpack-runtime-46b4b9908abd45fb29bc.js"
   },
   {
     "url": "styles.b72c1fc016bb9135624f.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8729846f7308ddf4de54b2c74ea85655"
+    "revision": "aa0a8aeea642035d6678525c442a6f87"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
